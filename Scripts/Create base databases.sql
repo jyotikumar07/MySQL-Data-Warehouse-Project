d7ca2bcfcec05ad0227b7bcbee20bc69a59ee0a4
Jyoti Sharma <jssimmisharma@gmail.com>
2025-11-14 16:59:02 -0600

    Added creating database script

---
+++ Scripts/Create base databases.sql
@@ -0,0 +1,10 @@
+
+Drop database if exists bronze;
+Drop database if exists silver;
+Drop database if exists gold;
+
+Create database bronze;
+Create database silver;
+Create database gold;
+
+Show databases;
